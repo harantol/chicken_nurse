4db8bea0c86c045a9541f6220ff18f7d8468e62b
Olivier <olivier.harant@gmail.com>
2025-03-12 21:31:59 +0100

    add global exception for opening in case of

--- micro_python/main.py
+++ micro_python/main.py
@@ -1,5 +1,10 @@
 from chicken_nurse import ChickenNurse
+from commandes_moteur import force_open_door
 if __name__ == "__main__":
-    chik = ChickenNurse(debug=False, use_deep_sleep=False, verbose=True)
-    chik.run()
+    try:
+        chik = ChickenNurse(debug=False, use_deep_sleep=False, verbose=True)
+        chik.run()
+    except:
+        print(f'Exception ! open door !')
+        force_open_door()
 
