0aee5f1c8c46c3bbfab5c54cf485ccd2e60b6831
Olivier <olivier.harant@gmail.com>
2025-03-16 11:59:54 +0100

    mod wlan_connextion for trying a list of ssid wifi.

--- micro_python/wlan_connection.py
+++ micro_python/wlan_connection.py
@@ -10,18 +10,29 @@ def disconnect(wlan: network.WLAN):
     time.sleep_ms(100)
 
 
-def connnect(verbose=True) -> network.WLAN:
+def connnect(verbose=True, ssid_pwd: [str, str] = [
+    ("Sosh", "PTit3F33"),
+    ("TitFeeTel", "Montagne09!")
+]) -> network.WLAN:
     wlan = network.WLAN(network.STA_IF)
     wlan.active(True)
-    wlan.connect("Sosh", "PTit3F33")
-    wait = 100
-    while wait > 0:
-        if wlan.status() < 0 or wlan.status() >= 3:
-            break
-        wait -= 1
+    for (ssid, pwd) in ssid_pwd:
         if verbose:
-            print('waiting for connection')
-        time.sleep(1)
+            print(f"Connecting to {ssid}...")
+        wlan.connect(ssid, pwd)
+        wait_max = 10
+        wait = wait_max
+        while wait > 0:
+            if wlan.status() < 0 or wlan.status() >= 3:
+                if wlan.status() == 3:
+                    if verbose:
+                        print('connected')
+                        print('IP: ', wlan.ifconfig()[0])
+                    return wlan
+            wait -= 1
+            if verbose:
+                print(f'waiting for connection {wait}/{wait_max}')
+            time.sleep(1)
     if wlan.status() != 3:
         raise RuntimeError('wifi connexion failed')
     else:
@@ -29,3 +40,17 @@ def connnect(verbose=True) -> network.WLAN:
             print('connected')
             print('IP: ', wlan.ifconfig()[0])
     return wlan
+
+
+def debug_wlan_connection(ssid_pwd=[
+    ("Sosh", "PTit3F33"),
+    ("TitFeeTel", "Montagne09!")
+], verbose: bool = False):
+    import network
+    import requests
+    wlan = connnect(ssid_pwd=ssid_pwd, verbose=verbose)
+    time.sleep(5)
+    www = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd"
+    print('IP: ', wlan.ifconfig()[0])
+    r = requests.get(www)
+    print(r.content)
