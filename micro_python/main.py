008b8871b257385cff91e58eaa85a22990d4b60b
Olivier <olivier.harant@gmail.com>
2025-03-16 12:00:05 +0100

    add a debug script in main

--- micro_python/main.py
+++ micro_python/main.py
@@ -1,10 +1,21 @@
 from chicken_nurse import ChickenNurse
-from commandes_moteur import force_open_door
-if __name__ == "__main__":
-    try:
-        chik = ChickenNurse(debug=False, use_deep_sleep=False, verbose=True)
-        chik.run()
-    except:
-        print(f'Exception ! open door !')
-        force_open_door()
+from commandes_moteur import open_door
 
+DEBUG: bool = False
+if __name__ == "__main__":
+    if not DEBUG:
+        try:
+            chik = ChickenNurse(debug=False, use_deep_sleep=False, verbose=True)
+            chik.run()
+        except:
+            print(f'Exception ! open door !')
+            open_door()
+    else:
+        from wlan_connection import debug_wlan_connection
+
+        ssid_pwd = [
+            ("Androliv", "PTit3F33"),
+            ("TitFeeTel", "Montagne09!")
+        ]
+
+        debug_wlan_connection(ssid_pwd=ssid_pwd, verbose=True)
