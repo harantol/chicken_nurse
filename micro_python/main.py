c753226f6aa53db0cdba9a7e78da2ff05edd7033
Olivier <olivier.harant@gmail.com>
2025-04-08 21:28:11 +0200

    write exception file in case of main failing

--- micro_python/main.py
+++ micro_python/main.py
@@ -1,15 +1,20 @@
 from chicken_nurse import ChickenNurse
 from commandes_moteur import open_door
-
+import os
 DEBUG: bool = False
+from machine import RTC
 if __name__ == "__main__":
     if not DEBUG:
         try:
             chik = ChickenNurse(debug=False, use_deep_sleep=False, verbose=True)
             chik.run()
         except:
+            t = RTC().datetime()
+            with open(f"exception_{t}.txt", "w") as file:
+                file.write('Exception ! open door !')
             print(f'Exception ! open door !')
             open_door()
     else:
         chik = ChickenNurse(debug=True, use_deep_sleep=False, verbose=True)
         chik.run()
+
