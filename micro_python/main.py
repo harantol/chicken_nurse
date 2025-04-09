48bde1465ed7736fc53d4123a5c44031550ab7d0
Olivier <olivier.harant@gmail.com>
2025-04-09 21:51:34 +0200

    put the no debug mode into a while true loop...

--- micro_python/main.py
+++ micro_python/main.py
@@ -5,15 +5,16 @@ DEBUG: bool = False
 from machine import RTC
 if __name__ == "__main__":
     if not DEBUG:
-        try:
-            chik = ChickenNurse(debug=False, use_deep_sleep=False, verbose=True)
-            chik.run()
-        except:
-            t = RTC().datetime()
-            with open(f"exception_{t}.txt", "w") as file:
-                file.write('Exception ! open door !')
-            print(f'Exception ! open door !')
-            open_door()
+        while True:
+            try:
+                chik = ChickenNurse(debug=False, use_deep_sleep=False, verbose=True)
+                chik.run()
+            except:
+                t = RTC().datetime()
+                with open(f"exception_{t}.txt", "w") as file:
+                    file.write('Exception ! open door !')
+                print(f'Exception ! open door !')
+                open_door()
     else:
         chik = ChickenNurse(debug=True, use_deep_sleep=False, verbose=True)
         chik.run()
